517d3e5758f3cf4ad92411ef7589da79396c6f95
xrw67 <xrw67@outlook.com>
2023-07-19 14:51:12 +0000

    add json

---
+++ bbt/util/json_test.cc
@@ -0,0 +1,77 @@
+#include "gmock/gmock.h"
+
+#include <vector>
+
+#include "bbt/util/json.h"
+#include "bbt/util/string_view.h"
+
+namespace {
+
+static const char* text =
+    "{\n"
+    "  \"answer\": {\n"
+    "    \"everything\": 42\n"
+    "  },\n"
+    "  \"happy\": true,\n"
+    "  \"list\": [\n"
+    "    1,\n"
+    "    0,\n"
+    "    2\n"
+    "  ],\n"
+    "  \"name\": \"Niels\",\n"
+    "  \"nothing\": null,\n"
+    "  \"object\": {\n"
+    "    \"currency\": \"USD\",\n"
+    "    \"value\": 42.99\n"
+    "  },\n"
+    "  \"pi\": 3.141\n"
+    "}";
+
+TEST(Json, Parse) {
+  bbt::json j = bbt::json::parse(text);
+
+  ASSERT_TRUE(j.is_object());
+
+  ASSERT_EQ(j["pi"], 3.141);
+  ASSERT_EQ(j["happy"], true);
+  ASSERT_EQ(j["name"], "Niels");
+  ASSERT_EQ(j["nothing"], nullptr);
+  ASSERT_EQ(j["answer"]["everything"], 42);
+
+  ASSERT_TRUE(j.contains("list"));
+  const auto& j_list = j["list"];
+  ASSERT_TRUE(j_list.is_array());
+  ASSERT_EQ(j_list[0], 1);
+  ASSERT_EQ(j_list[1], 0);
+  ASSERT_EQ(j_list[2], 2);
+
+  ASSERT_EQ(j["object"]["currency"], "USD");
+  ASSERT_EQ(j["object"]["value"], 42.99);
+}
+
+TEST(Json, Serializer) {
+  bbt::json j;
+
+  j["pi"] = 3.141;
+  j["happy"] = true;
+  j["name"] = "Niels";
+  j["nothing"] = nullptr;
+  j["answer"]["everything"] = 42;
+  j["list"] = std::vector<int>({1, 0, 2});
+  j["object"]["currency"] = "USD";
+  j["object"]["value"] = 42.99;
+
+  std::string serialized = j.dump(2, ' ');
+  ASSERT_EQ(serialized, text);
+}
+
+TEST(Json, StingView) {
+  bbt::json j;
+
+  const char* val = "value666";
+  j["name"] = bbt::string_view(val, 5);
+  std::string serialized = j.dump();
+  ASSERT_EQ(serialized, "{\"name\":\"value\"}");
+}
+
+}  // namespace
---
+++ include/bbt/util/json.h
@@ -0,0 +1,17 @@
+/**
+ * JSON for Modern C++
+ * https://github.com/nlohmann/json
+ */
+
+#ifndef BBT_UTIL_JSON_H_
+#define BBT_UTIL_JSON_H_
+
+#include "internal/json/json.hpp"
+
+namespace bbt {
+
+using nlohmann::json;
+
+}
+
+#endif  // BBT_UTIL_JSON_H_
