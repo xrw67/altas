bf775a5b2c5ea33cb40aad51638d6f0343d4af3a
xrw67 <xrw67@outlook.com>
2023-07-26 09:54:06 +0000

    update gtest to 1.12.1

--- tests/test_hello.cc
+++ tests/test_hello.cc
@@ -3,6 +3,27 @@
 
 using namespace bbt;
 
-TEST(Hello, World) {
-    ASSERT_TRUE(true);
-}
+struct Turtle {
+  virtual ~Turtle() {}
+  virtual void PenUp() = 0;
+  virtual void PenDown() = 0;
+  virtual void Forward(int distance) = 0;
+  virtual void Turn(int degrees) = 0;
+  virtual void GoTo(int x, int y) = 0;
+  virtual int GetX() const = 0;
+  virtual int GetY() const = 0;
+};
+
+namespace testing {
+struct MockTurtle : public Turtle {
+  MOCK_METHOD(void, PenUp, (), (override));
+  MOCK_METHOD(void, PenDown, (), (override));
+  MOCK_METHOD(void, Forward, (int distance), (override));
+  MOCK_METHOD(void, Turn, (int degrees), (override));
+  MOCK_METHOD(void, GoTo, (int x, int y), (override));
+  MOCK_METHOD(int, GetX, (), (const, override));
+  MOCK_METHOD(int, GetY, (), (const, override));
+};
+}  // namespace testing
+
+TEST(Hello, World) { ASSERT_TRUE(true); }
