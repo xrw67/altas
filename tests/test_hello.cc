7d0bf202716fba7edae6d794d669f4ed8889967b
xrw67 <xrw67@outlook.com>
2023-07-26 10:53:26 +0000

    add MockTest.Example

--- tests/test_hello.cc
+++ tests/test_hello.cc
@@ -3,27 +3,33 @@
 
 using namespace bbt;
 
-struct Turtle {
-  virtual ~Turtle() {}
-  virtual void PenUp() = 0;
-  virtual void PenDown() = 0;
-  virtual void Forward(int distance) = 0;
-  virtual void Turn(int degrees) = 0;
-  virtual void GoTo(int x, int y) = 0;
-  virtual int GetX() const = 0;
-  virtual int GetY() const = 0;
+namespace {
+
+struct DB {
+  virtual void Query(const char* sql) = 0;
 };
 
-namespace testing {
-struct MockTurtle : public Turtle {
-  MOCK_METHOD(void, PenUp, (), (override));
-  MOCK_METHOD(void, PenDown, (), (override));
-  MOCK_METHOD(void, Forward, (int distance), (override));
-  MOCK_METHOD(void, Turn, (int degrees), (override));
-  MOCK_METHOD(void, GoTo, (int x, int y), (override));
-  MOCK_METHOD(int, GetX, (), (const, override));
-  MOCK_METHOD(int, GetY, (), (const, override));
+struct MockDB : public DB {
+  MOCK_METHOD(void, Query, (const char* sql), (override));
 };
-}  // namespace testing
 
-TEST(Hello, World) { ASSERT_TRUE(true); }
+struct DBClient {
+  DB* db;
+
+  explicit DBClient(DB* db) : db(db) {}
+
+  void Do() { db->Query("SELECT * FROM t1;"); }
+};
+
+using ::testing::AtLeast;
+
+TEST(MockTest, Example) {
+  MockDB mock;
+  DBClient client(&mock);
+
+  EXPECT_CALL(mock, Query("SELECT * FROM t1;")).Times(AtLeast(1));
+
+  client.Do();
+}
+
+}  // namespace
